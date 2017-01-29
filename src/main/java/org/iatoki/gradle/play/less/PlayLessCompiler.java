525fdca6ba9d1067515aab96768763792c4f8baf
Ashar Fuadi <fushar@gmail.com>
2017-01-29 21:17:50 +0000

    Change implementation to less4j

--- src/main/java/org/iatoki/gradle/play/less/PlayLessCompiler.java
+++ src/main/java/org/iatoki/gradle/play/less/PlayLessCompiler.java
@@ -1,10 +1,8 @@
 package org.iatoki.gradle.play.less;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.FileVisitDetails;
@@ -12,37 +10,40 @@ import org.gradle.api.file.FileVisitor;
 import org.gradle.api.file.RelativePath;
 import org.gradle.api.internal.file.RelativeFile;
 import org.gradle.api.tasks.WorkResult;
-import org.lesscss.LessCompiler;
-import org.lesscss.LessException;
+import org.gradle.util.GFileUtils;
+
+import com.github.sommeri.less4j.Less4jException;
+import com.github.sommeri.less4j.LessCompiler;
+import com.github.sommeri.less4j.LessCompiler.CompilationResult;
+import com.github.sommeri.less4j.LessCompiler.Configuration;
+import com.github.sommeri.less4j.LessSource;
+import com.github.sommeri.less4j.MultiPathFileSource;
+import com.github.sommeri.less4j.core.ThreadUnsafeLessCompiler;
 
 class PlayLessCompiler {
     private static final String LESS_EXT = ".less";
     private static final String CSS_EXT = ".css";
 
     WorkResult compile(LessCompileSpec spec) {
-        List<String> args = new ArrayList<>();
-
-        args.add("--compress");
-
-        if (!spec.getOptions().getIncludePaths().isEmpty()) {
-            String paths = spec.getOptions().getIncludePaths()
-                    .stream()
-                    .map(File::getAbsolutePath)
-                    .map(path -> "\"" + path + "\"")
-                    .collect(Collectors.joining(";"));
-            args.add("--include-path=" + paths);
-        }
-
-        LessCompiler compiler = new LessCompiler(args);
+        LessCompiler compiler = new ThreadUnsafeLessCompiler();
 
         for (RelativeFile lessFile : toRelativeFiles(spec.getSourceFiles())) {
             File cssFile = new File(
                     spec.getDestinationDirectory(),
                     toCss(lessFile.getRelativePath()).getPathString());
 
+            LessSource lessSource = new MultiPathFileSource(
+                    lessFile.getFile(),
+                    spec.getOptions().getIncludePaths().toArray(new File[0]));
+
+            Configuration options = new Configuration();
+            options.setCssResultLocation(cssFile);
+            options.setCompressing(true);
+
             try {
-                compiler.compile(lessFile.getFile(), cssFile);
-            } catch (IOException | LessException e) {
+                CompilationResult result = compiler.compile(lessSource, options);
+                GFileUtils.writeFile(result.getCss(), cssFile);
+            } catch (Less4jException e) {
                 throw new RuntimeException(e);
             }
         }
